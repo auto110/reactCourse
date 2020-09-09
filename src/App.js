8bc836372872390dbc1a5b5b6c85a1b0958597fe
qiaolin chen <itqiaolin@gmail.com>
2020-09-09 09:22:12 +0800

    写一个原型页面

--- src/App.js
+++ src/App.js
@@ -24,10 +24,36 @@ const Greeting = props => <p>Hello {props.name}!</p>;
 // const App = () => <div>Hello React!</div>;
 
 class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      passwordConfirmation: '',
+      email: '',
+      errors: []
+    };
+  }
+  
+  displayForm() {
+    return (
+      <div>
+        Username: <input type="text" /><br />
+        Password: <input type="text" /><br />
+        Password Confirmation: <input type="text" /><br />
+        Email: <input type="text" /><br />
+        <br />
+        <button>Submit</button>
+      </div>
+    );
+  }
+  
   render(){
     return (
       <div className = "App">
-        Test App
+        Create Account 
+        <hr/>
+        {this.displayForm()}
       </div>
     )
   }
