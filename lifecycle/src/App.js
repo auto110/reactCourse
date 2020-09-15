505b18da59ee817494c21f8e3ac8b43893cb1933
qiaolin chen <itqiaolin@gmail.com>
2020-09-15 11:16:23 +0800

    改造成类组件

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    console.log("Constructor")
+  }
+
+  render(){
+    return (
+      <div className="App">react生命周期</div>
+    )
+  }
 }
 
 export default App;
