905f071821e72399bc700aa98bee6c753ce857cf
qiaolin chen <itqiaolin@gmail.com>
2020-09-16 10:18:51 +0800

    新建Profile组件

---
+++ dataflow-classcomponent/src/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+// Create a file named Profile.js and add a class component that returns some JSX in the render function
+
+class Profile extends Component {
+    render(){
+        return (
+            <section className="profile">
+                Profile
+            </section>
+        )
+    }
+}
+
+export default Profile;
