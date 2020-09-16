1c7efd2da8cf726fb338de17ed569eefb338814f
qiaolin chen <itqiaolin@gmail.com>
2020-09-16 10:24:11 +0800

    作为Profile组件的属性，将数据传递给Profile组件

--- dataflow-classcomponent/src/App.js
+++ dataflow-classcomponent/src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 import Profile from "./Profile"
 
+const user = {
+  name: "Brian",
+  interests: "Reading, Swimming, Technology",
+  age: 9,
+  image: "https://unsplash.com/photos/g4PLFkpUf4Q",
+  color: "Red",
+  movie: "Star Wars"
+};
+
 function App() {
   return (
     <div className="App">
-      <Profile/>
+      <Profile user = {user}/>
     </div>
   );
 }
