d1c2e53d00f114e3ef23329a22fea1edefd773dd
qiaolin chen <itqiaolin@gmail.com>
2020-09-16 10:32:14 +0800

    在Profile组件中读取父组件传递给Profile组件的数据

--- dataflow-classcomponent/src/Profile.js
+++ dataflow-classcomponent/src/Profile.js
@@ -4,9 +4,22 @@ import React, { Component } from "react";
 
 class Profile extends Component {
     render(){
+        const { name, image, age, interests, color, movie} = this.props.user;
+
         return (
             <section className="profile">
                 Profile
+                <header>
+                    <h1> {this.props.user.name}</h1>
+                </header>
+                <div className = "profile-content">
+                <div className = "profile-image">
+                    <img src = { this.props.user.image} alt= {this.props.user.name}></img>
+                </div>
+                <p>
+                    <strong>Age:</strong> {this.props.user.age}
+                </p>
+                </div>
             </section>
         )
     }
