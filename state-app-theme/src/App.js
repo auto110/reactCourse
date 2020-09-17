4d86adf5502f521da62135241a7d80d3ff5dce55
qiaolin chen <itqiaolin@gmail.com>
2020-09-17 09:16:24 +0800

    项目初始化npx create-react-app state-app-theme

---
+++ state-app-theme/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      theme: "light"
+    }
+  }
+
+  render(){
+    return(
+      
+    )
+  }
+}
+
+export default App;
