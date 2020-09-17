bee2c09dcf0346dd93dfd566c05ad10b68f0bfe5
qiaolin chen <itqiaolin@gmail.com>
2020-09-17 09:18:44 +0800

    定义控制样式主题的状态字段

--- state-app-theme/src/App.js
+++ state-app-theme/src/App.js
@@ -5,13 +5,14 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
+      // define the value for the theme in the state of the component, the value of which can be used to change the css class of the div wrapper element. Initialize state with the value for theme set to light
       theme: "light"
     }
   }
 
   render(){
     return(
-      
+
     )
   }
 }
