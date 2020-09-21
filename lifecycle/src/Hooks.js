d7f112227b17980459307b1de77556d7f916ad9a
qiaolin chen <itqiaolin@gmail.com>
2020-09-21 11:15:49 +0800

    he useEffect hook runs after every render.
    When using functions, we don't have class component life cycle methods. Instead, there is a hook called useEffect that we can use to get the componentDidMount, componentDidUpdate, and componentWillUnmount mechanisms. When used, the useEffect hook runs after every render.

--- lifecycle/src/Hooks.js
+++ lifecycle/src/Hooks.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+
+    // Called after every render
+    useEffect(() => {
+        console.log('Counter value is now ' + count)
+    });
+    
     return (
         <div>
           <p>{count}</p>
