dc1723cfedf12efb88c87e9172e99050647549c7
qiaolin chen <itqiaolin@gmail.com>
2020-09-22 19:05:54 +0800

    使用 Material-UI 组件入门
    Signed-off-by: qiaolin chen <itqiaolin@gmail.com>

--- shoppinglist/src/App.js
+++ shoppinglist/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
-  
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant='h6' color='inherit'>
+            Shopping Cart
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
