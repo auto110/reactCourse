a1f6a7523bb1203ea96acf135a747653a5841d34
qiaolin chen <itqiaolin@gmail.com>
2020-09-22 19:51:18 +0800

    路由的简单用法示例
    Signed-off-by: qiaolin chen <itqiaolin@gmail.com>

--- shoppinglist/src/App.js
+++ shoppinglist/src/App.js
@@ -10,6 +10,10 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import Contact from './Contact';
+import Home from './Home';
+
 function App() {
   const [items, setItems] = React.useState([]);
   const addItem = (item) => {
@@ -24,13 +28,29 @@ function App() {
 
   return (
     <div className="App">
-      <AppBar position="static" color="default">
+     <AppBar position="static" color="default">
         <Toolbar>
           <Typography variant='h6' color='inherit'>
             Shopping Cart
           </Typography>
         </Toolbar>
       </AppBar>
+
+    {/* Routing */}
+    <BrowserRouter>
+        <div>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/contact">Contact</Link>{' '} 
+          <Link to="/links">Links</Link>{' '} 
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/links" render={() => <h1>Links</h1>} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+          </div>
+      </BrowserRouter> 
+
       <AddItem addItem={addItem} /> 
       <List>{listItems}</List>
     </div>
