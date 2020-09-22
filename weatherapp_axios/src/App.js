148e536d2354706b1d55f94849167374ae28fbc2
qiaolin chen <itqiaolin@gmail.com>
2020-09-22 10:00:45 +0800

    获取github数据组件
    Signed-off-by: qiaolin chen <itqiaolin@gmail.com>

--- weatherapp_axios/src/App.js
+++ weatherapp_axios/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import WeatherApp from './WeatherApp'
+import GithubApp from './GithubApp'
 
 function App() {
   return (
     <div className="App">
       <WeatherApp />
+      <GithubApp/>
     </div>
   );
 }
