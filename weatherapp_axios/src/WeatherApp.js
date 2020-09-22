e046f0eff4ed0bbc0d11efbe7d87736b1d8ccf69
qiaolin chen <itqiaolin@gmail.com>
2020-09-22 09:33:33 +0800

    添加获取数据接口并渲染组件
    Signed-off-by: qiaolin chen <itqiaolin@gmail.com>

--- weatherapp_axios/src/WeatherApp.js
+++ weatherapp_axios/src/WeatherApp.js
@@ -11,9 +11,37 @@ class WeatherApp extends Component{
             loading: true
         }
     }
+
+    componentDidMount(){
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&APIkey=a25f869e27704fa1f6690ae5c3def1cf')
+        .then(response=>response.json())
+        .then(responseData=>{
+            this.setState({
+                temp: responseData.main.temp,
+                desc: responseData.weather[0].description,
+                icon: responseData.weather[0].icon, 
+                loading: false
+            })
+        })
+        .catch(err => console.error(err))
+    }
+
     render(){
-        return (
-            <div className="App"></div>
-        )
+        const imgSrc = `http://openweathermap.org/img/w/${this.state.icon}.png`;
+        console.log(imgSrc);
+
+        if(this.state.loading){
+            return <p>Loading...</p>
+        }else{
+            return (
+                <div className="App">
+                    <p>Temperature: {this.state.temp} °C</p>
+                    <p>Description: {this.state.desc}</p>
+                    <img src={imgSrc} alt="Weather icon" />
+                </div>
+            )
+        }
     }
 }
+
+export default WeatherApp;
