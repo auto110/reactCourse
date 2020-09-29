7253dfd91c9218d840fdf76ac06b06d3350ebbaf
qiaolin chen <itqiaolin@gmail.com>
2020-09-29 14:25:44 +0800

    表单组件
    Signed-off-by: qiaolin chen <itqiaolin@gmail.com>

--- carfront/src/components/Carlist.js
+++ carfront/src/components/Carlist.js
@@ -1,4 +1,4 @@
-import React, {component} from 'react'
+import React, {Component} from 'react'
 
 class Carlist extends Component {
     constructor(props){
@@ -9,9 +9,32 @@ class Carlist extends Component {
         }
     }
   
+    componentDidMount(){
+        fetch('http://localhost:8081/api/cars')
+        .then((response) => {
+            response.json()
+        })
+        .then((responseData) =>{
+            this.setState({
+                cars: responseData._embeeded.cars
+            })
+        })
+    }
+
     render() {
+        const tableRows = this.state.cars.map((car, index)=>
+            <tr key={index}>
+            <td>{car.brand}</td>
+            <td>{car.model}</td>
+            <td>{car.color}</td>
+            <td>{car.year}</td>
+            <td>{car.price}</td>
+            </tr>
+        );
       return (
-        <div></div>
+        <div>
+            <tbody>{tableRows}</tbody>
+        </div>
       );
     }
 }
